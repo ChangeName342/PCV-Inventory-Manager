87edc2480c356be7c2dfb9e86e2137d52f31bd03
Ignacio Barrientos <132976388+ChangeName342@users.noreply.github.com>
2025-08-29 22:37:38 -0400

    Configuracion conexión Postgresql

--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
+// Configurar CORS
 const corsOptions = {
-    origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173"],
 };
-
 app.use(cors(corsOptions));
+app.use(express.json()); // Para futuras rutas que reciban JSON
 
-
+// Rutas de prueba
 app.get("/api", (req, res) => {
-    res.json({ fruits: ["apple", "orange", "banana"] });
+  res.json({ fruits: ["apple", "orange", "banana"] });
 });
 
 app.listen(8080, () => {
-    console.log("Server started on port 8080");
+  console.log("Server started on port 8080");
 });
